7d44c4dd7f12b1819790822f119a14f78d0727a8
tpszhao <tonyzhao111@gmail.com>
2020-02-04 10:54:14 -0800

    removed switch block

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,30 +6,33 @@ import './Card.css'
 
 export default function Card({winner,updateScoreList}) {
     const [user, setUser] = useState(null);
-    const [status, setStatus] = useState("initial");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
     const [value, setValue] = useState("");
     const cancel = useRef(null);
 
     useEffect(() => {
-        if(status !== "success"){
+        if(loading || error){
             updateScoreList("empty");
-        }else if(user.public_repos && user.followers){
-            console.log("updated")
+        }else if(user&&user.public_repos&&user.followers){
             updateScoreList(user.public_repos + user.followers);
         }
-    }, [user,status])
+    }, [user,loading,error])
 
     const search = e => {
         cancel.current && cancel.current();
         e.preventDefault();
-        setStatus("loading");
+        setLoading(true);
+        setError(false);
+        setUser(null);
         axios.get(`https://api.github.com/users/${value}`,{
             cancelToken:new axios.CancelToken(c => cancel.current = c)
         }).then(res=>{
+            setLoading(false);
             setUser(res.data);
-            setStatus("success");
         }).catch(() =>{
-            setStatus("error");
+            setError(true);
+            setLoading(false);
         })
     }
 
@@ -38,27 +41,14 @@ export default function Card({winner,updateScoreList}) {
         setValue(username);
     }
 
-    const displayinfo = ()=>{
-        switch(status){
-            case "initial":
-                return <span>Waiting For Input</span>
-            case "loading":
-                return <span>Loading</span>
-            case "success":
-                return user&&<UserStat user = {user} winner={winner}/>
-            case "error":
-                return <span>User Does Not Exist</span>
-            default:
-                return;
-        }
-    }
-
     return (
         <div className="card">
             <form onSubmit={search}>
                 <input type="text" value={value} onChange={changeValue}/>
             </form>
-            {displayinfo()}
+            {loading&&<span>Loading</span>}
+            {!loading&&error&&<span>Error</span>}
+            {!loading&&user&&<UserStat user = {user} winner={winner}/>}
         </div>
     )
 }
