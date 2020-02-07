981f6d1c8919637385e3ba65d01434036fc9d98c
tpszhao <tonyzhao111@gmail.com>
2020-02-07 13:18:11 -0800

    initial commit

--- src/components/CloseButton.js
+++ src/components/CloseButton.js
@@ -2,16 +2,9 @@ import React from 'react'
 import styles from './CloseButton.module.css'
 
 export default function CloseButton({style={},onClick=null}) {
-    const {background,top,bottom,left,right,center,transform} = styles;
+    const {background} = styles;
     return (
         <div className={background} onClick={onClick} style={style}>
-            <div className={transform}>
-                <div className={top}></div>
-                <div className={bottom}></div>
-                <div className={center}></div>   
-                <div className={right}></div> 
-                <div className={left}></div>        
-            </div>
         </div>
     )
 }
