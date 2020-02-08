bd375da31a605c5a6c0a307f460ed31eb2745751
tpszhao <tonyzhao111@gmail.com>
2020-02-07 20:07:47 -0800

    resolved conflict

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -8,11 +8,7 @@ import styles from './Card.module.css'
 
 
 export default function Card(props) {
-<<<<<<< HEAD:src/components/Card/Card.js
     const {winner=false,user,updateUser,removeCard} = props;
-=======
-    const {winner=false,user=null,idx,updateUser,removeCard} = props;
->>>>>>> master:src/components/Card.js
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
     const [value, setValue] = useState("");
