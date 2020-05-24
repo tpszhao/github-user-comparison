7ec023753d577f181b23cb6f08076659c8d500b8
tpszhao <tonyzhao111@gmail.com>
2020-05-24 14:21:19 -0700

    stuff

--- src/App.js
+++ src/App.js
@@ -1,73 +1,74 @@
-import React, {useState,useMemo} from 'react';
-import {Card,BlankCard} from './components'
-import './App.css';
-
+import React, { useState, useMemo } from "react";
+import { Card, BlankCard } from "./components";
+import "./App.css";
 
 function App() {
-  const [userList, setUserList] = useState([Symbol(),Symbol()]);
+  const [userList, setUserList] = useState([Symbol(), Symbol()]);
 
-  const updateUser = (idx,user)=>{
+  const updateUser = (idx, user) => {
     let newlist = userList.slice();
-    newlist.splice(idx,1,user);
+    newlist.splice(idx, 1, user);
     setUserList(newlist);
-  }
+  };
 
-  const userScore = user=>{
+  const userScore = (user) => {
     let score;
-    switch(typeof user){
+    switch (typeof user) {
       case "symbol":
+        console.log("test");
         score = -1;
         break;
       default:
-        score = user.public_repos + user.followers
+        score = user.public_repos + user.followers;
     }
-    return score
-  }
+    return score;
+  };
 
-  const determineWinner = ()=>{
+  const determineWinner = () => {
     if (userList.length < 2) return null;
     let newlist = userList.slice();
-    newlist.sort((a,b)=>userScore(b) - userScore(a));
+    newlist.sort((a, b) => userScore(b) - userScore(a));
     let firstUser = newlist[0];
     let secondUser = newlist[1];
     let winner = null;
 
-    switch(userScore(secondUser)){
+    switch (userScore(secondUser)) {
       case -1:
       case userScore(firstUser):
         break;
       default:
-        winner = firstUser&&firstUser.login;
+        winner = firstUser && firstUser.login;
     }
-    return winner
-  }
+    return winner;
+  };
 
   const winner = useMemo(() => determineWinner(), [userList]);
 
-  const addCard = ()=>{
+  const addCard = () => {
     let newlist = userList.slice();
     newlist.push(Symbol());
     setUserList(newlist);
-  }
+  };
 
-  const removeCard = user =>{ 
-    let newlist = userList.filter(item=>item!==user);
+  const removeCard = (user) => {
+    let newlist = userList.filter((item) => item !== user);
     setUserList(newlist);
-  }
+  };
 
   return (
     <div className="container">
-      {
-        userList.map((user,i)=>{
-          return <Card 
-                    winner={winner} 
-                    user={user} 
-                    key={i} 
-                    removeCard={removeCard}
-                    updateUser={user=>updateUser(i,user)}/>
-        })
-      }
-      <BlankCard onClick={addCard}/>
+      {userList.map((user, i) => {
+        return (
+          <Card
+            winner={winner}
+            user={user}
+            key={i}
+            removeCard={removeCard}
+            updateUser={(user) => updateUser(i, user)}
+          />
+        );
+      })}
+      <BlankCard onClick={addCard} />
     </div>
   );
 }
